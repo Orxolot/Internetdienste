6bb12143db5281a1601517fa9aaf090ef75cdcbf
Orxolot <tolu@lknet.ch>
2023-07-15 16:48:20 +0200

    Update server.js
    Scheiss auf die E-Mail, ich speichere nun die Kontaktangaben und bestellung beide auf dem System ab. funktioniert noch nicht, kommt aber noch.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const nodemailer = require('nodemailer');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const crypto = require('crypto');
 
 const app = express();
 const port = 3000;
@@ -15,59 +13,22 @@ app.use(bodyParser.text({ type: 'application/xml' }));
 
 // Endpunkt für das Verarbeiten des Kontaktformulars
 app.post('/kontakt', (req, res) => {
-  const { vorname, nachname, email, nachricht } = req.body;
-
-  // E-Mail-Konfiguration
-  const transporter = nodemailer.createTransport({
-    host: 'mail.gmx.net',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'tolu_lknet@gmx.ch',
-      pass: 'a9408a3c0ea51c7de6d37394cbe2c32a41e8f99453' // Verwenden Sie das verschlüsselte Passwort hier
-    },
-    requireTLS: true
-  });
-  
-
-  // E-Mail-Inhalt
-  const mailOptions = {
-    from: 'tolu_lknet@gmx.ch', // Absender-E-Mail-Adresse
-    to: 'tolu@lknet.ch', // Empfänger-E-Mail-Adresse
-    subject: 'Kontaktanfrage von LKNet', // Betreff der E-Mail
-    text: `
-      Vorname: ${vorname}
-      Nachname: ${nachname}
-      E-Mail: ${email}
-      
-      Nachricht:
-      ${nachricht}
-    `
-  };
-  // E-Mail senden
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Fehler beim Senden der Kontakt-E-Mail.', error);
-      res.status(500).send('Fehler beim Senden der Kontakt-E-Mail.');
-    } else {
-      console.log('Kontakt-E-Mail erfolgreich gesendet.');
-      res.status(200).send('Kontakt-E-Mail erfolgreich gesendet.');
-    }
-  });
-});
-
-// Endpunkt für das Speichern des Bestellformulars
-app.post('/bestellung', (req, res) => {
   const xmlData = req.body;
+  const email = req.body.email; // Annahme: Das Feld "email" enthält die E-Mail-Adresse
+
+  // Erstellen Sie den Dateinamen basierend auf der E-Mail-Adresse
+  const fileName = email.replace(/[^a-z0-9]/gi, '_') + '.xml';
 
   // Speichern Sie das XML auf dem Server
-  fs.writeFile('pfad/zur/bestellung.xml', xmlData, (err) => {
+  const filePath = '\\\\SRV01\\contact\\' + fileName; // Beachten Sie die doppelten Backslashes
+
+  fs.writeFile(filePath, xmlData, (err) => {
     if (err) {
-      console.error('Fehler beim Speichern der Bestellungs-XML.', err);
-      res.status(500).send('Fehler beim Speichern der Bestellungs-XML.');
+      console.error('Fehler beim Speichern des Kontakt-XML.', err);
+      res.status(500).send('Fehler beim Speichern des Kontakt-XML.');
     } else {
-      console.log('Bestellungs-XML erfolgreich gespeichert.');
-      res.status(200).send('Bestellungs-XML erfolgreich gespeichert.');
+      console.log('Kontakt-XML erfolgreich gespeichert.');
+      res.status(200).send('Kontakt-XML erfolgreich gespeichert.');
     }
   });
 });
