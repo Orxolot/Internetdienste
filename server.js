bb61bed8afc9a090f3f738ae10e386d28f4fcb65
Orxolot <tolu@lknet.ch>
2023-08-21 20:24:46 +0200

    Update server.js
    Verbindung zur Datenbank aufgebaut

--- server.js
+++ server.js
@@ -8,72 +8,72 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Verbindung zur MongoDB-Datenbank herstellen
-mongoose.connect("mongodb://localhost:27017/your-database-name", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect("mongodb://root:example@10.0.5.10:27017/?authMechanism=DEFAULT", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Verbindungsfehler:"));
 db.once("open", () => {
-  console.log("Verbindung zur MongoDB hergestellt");
+    console.log("Verbindung zur MongoDB hergestellt");
 });
 
 // Mongoose-Schema für Benutzerdaten definieren
 const userSchema = new mongoose.Schema({
-  id: Number,
-  firstName: String,
-  lastName: String,
-  email: String,
-  message: String,
+    id: Number,
+    firstName: String,
+    lastName: String,
+    email: String,
+    message: String,
 });
 
 const User = mongoose.model("User", userSchema);
 
 app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    console.error("Fehler beim Abrufen der Benutzer:", error);
-    res.status(500).json({ message: "Interner Serverfehler" });
-  }
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error("Fehler beim Abrufen der Benutzer:", error);
+        res.status(500).json({ message: "Interner Serverfehler" });
+    }
 });
 
 app.get("/users/:id", async (req, res) => {
-  const userId = parseInt(req.params.id);
-  try {
-    const user = await User.findOne({ id: userId });
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: "Benutzer nicht gefunden" });
+    const userId = parseInt(req.params.id);
+    try {
+        const user = await User.findOne({ id: userId });
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: "Benutzer nicht gefunden" });
+        }
+    } catch (error) {
+        console.error("Fehler beim Abrufen des Benutzers:", error);
+        res.status(500).json({ message: "Interner Serverfehler" });
     }
-  } catch (error) {
-    console.error("Fehler beim Abrufen des Benutzers:", error);
-    res.status(500).json({ message: "Interner Serverfehler" });
-  }
 });
 
 app.post("/users", async (req, res) => {
-  const newUser = new User({
-    id: req.body.id,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    message: req.body.message,
-  });
+    const newUser = new User({
+        id: req.body.id,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        message: req.body.message,
+    });
 
-  try {
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error("Fehler beim Speichern des Benutzers:", error);
-    res.status(500).json({ message: "Interner Serverfehler" });
-  }
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error("Fehler beim Speichern des Benutzers:", error);
+        res.status(500).json({ message: "Interner Serverfehler" });
+    }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server läuft auf http://localhost:${PORT}`);
+    console.log(`Server läuft auf http://localhost:${PORT}`);
 });
