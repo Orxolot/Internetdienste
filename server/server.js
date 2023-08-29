8e97faaefa5dba33b2e8140588b69183267a672b
Orxolot <tolu@lknet.ch>
2023-08-29 20:43:05 +0200

    Update
    Weiteres Modul getestet, leider auch kein Erfolg

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors');
+app.use(cors());
 
 const app = express();
 const PORT = process.env.PORT || 3000;
