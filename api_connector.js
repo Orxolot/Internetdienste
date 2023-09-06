cd53468ce12a0dab347edab2b02c28032eca679f
Orxolot <tolu@lknet.ch>
2023-09-06 20:44:22 +0200

    Update api_connector.js
    Api connectot korrigier, da die Verbindung nicht aufgebaut werden konnte

--- api_connector.js
+++ api_connector.js
@@ -1,14 +1,13 @@
-// api_connector.js
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('kontakt-formular');
 
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
 
-        const vorname = form.vorname.value;
-        const nachname = form.nachname.value;
-        const email = form.email.value;
-        const nachricht = form.nachricht.value;
+        const vorname = form.querySelector('[name="vorname"]').value;
+        const nachname = form.querySelector('[name="nachname"]').value;
+        const email = form.querySelector('[name="email"]').value;
+        const nachricht = form.querySelector('[name="nachricht"]').value;
 
         const userData = {
             firstName: vorname,
